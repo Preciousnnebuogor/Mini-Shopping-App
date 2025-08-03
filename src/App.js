4b3a893266ff3dd99cbbe1e6d39b59cef50031ac
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2025-08-03 11:09:03 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Check from './components/check';
+import Home from './components/home';
+
+function App() {
+  return (
+    <div className="App">
+  {/* <Check/> */}
+  <Home/>
+
+    </div>
+  );
+}
+
+export default App;
